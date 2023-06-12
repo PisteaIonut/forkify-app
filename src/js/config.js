baedf65e5ba8db7abf012f8dae3f86ae9fb710b1
PisteaIonut <111272747+PisteaIonut@users.noreply.github.com>
2023-06-12 13:19:11 +0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '030c31c3-8c64-4878-9ea0-d610bb525bf2';
+export const MODAL_CLOSE_SEC = 2.5;
